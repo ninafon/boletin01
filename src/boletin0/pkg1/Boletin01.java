e2d86c5717f297909876bfc8fc37d11195da9db9
nina <nina@nina-VAIO>
2014-10-02 12:57:32 +0200

    saudo a dam14

--- src/boletin0/pkg1/Boletin01.java
+++ src/boletin0/pkg1/Boletin01.java
@@ -4,7 +4,7 @@ package boletin0.pkg1;
 public class Boletin01 {
     
     public static void main(String[] args) {
-        System.out.println("saudo\n a \tDAM 1");
+        System.out.println("saudo\n a \tDAM 14");
         System.out.println(" outro saudo");
     }
     
