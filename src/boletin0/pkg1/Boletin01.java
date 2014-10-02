4b1c5e375ac787bf5fd213d32e47cd86bdec4e47
nina <nina@nina-VAIO>
2014-10-02 12:56:28 +0200

    incio

---
+++ src/boletin0/pkg1/Boletin01.java
@@ -0,0 +1,11 @@
+  
+package boletin0.pkg1;
+
+public class Boletin01 {
+    
+    public static void main(String[] args) {
+        System.out.println("saudo\n a \tDAM 1");
+        System.out.println(" outro saudo");
+    }
+    
+}
